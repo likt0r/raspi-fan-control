650fa614e99acc1de7299474406d781dd07b374a
Benjamin Werner <benjamin.werner42@protonmail.com>
2020-11-29 14:55:55 +0000

    add service file and install instrcutrions

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # raspi-fan-control
 small script and service file to turn on fan if cpu is to hot
+
+# INSTALL instructions
+copy service file to /etc/systemd/system
+```bash
+sudo cp fan-control.service /etc/systemd/system/fan-control.service
+sudo systemctl start fan-control.service
+sudo systemctl enable fan-control.service
+```
--- main.py
+++ main.py
@@ -1,41 +1,38 @@
-
 #!/usr/bin/env python
 import RPi.GPIO as GPIO
 import time
 import os
 
 
-IMPULS_PIN		= 23	#Pin leading to transitor
-SLEEP_TIME		= 20	#Sleept time till next temperatur mesurement
-MAX_CPU_TEMP	= 40	#Temperatur cooler starts
-STOP_CPU_TEMP	= 35    #Temperatur cooler stops 
+IMPULS_PIN = 23  # Pin leading to transitor
+SLEEP_TIME = 20  # Sleept time till next temperatur mesurement
+MAX_CPU_TEMP = 45  # Temperatur cooler starts
+STOP_CPU_TEMP = 30  # Temperatur cooler stops
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 
 def get_cpu_temperature():
-	temp = os.popen('vcgencmd measure_temp').readline()
-	return float(temp.replace("temp=","").replace("'C\n",""))
-
+    temp = os.popen("vcgencmd measure_temp").readline()
+    return float(temp.replace("temp=", "").replace("'C\n", ""))
 
 
 def main():
 
-	#Init
-	GPIO.setup(IMPULS_PIN, GPIO.OUT)
-	GPIO.output(IMPULS_PIN, False)
-	 
-	while True:
-		cpu_temp = get_cpu_temperature()
-		print(cpu_temp)
-		if cpu_temp >= MAX_CPU_TEMP  :
-			GPIO.output(IMPULS_PIN, True)
-		if cpu_temp < STOP_CPU_TEMP:
-			GPIO.output(IMPULS_PIN, False)
-		time.sleep(SLEEP_TIME)
+    # Init
+    GPIO.setup(IMPULS_PIN, GPIO.OUT)
+    GPIO.output(IMPULS_PIN, False)
+
+    while True:
+        cpu_temp = get_cpu_temperature()
+        print(cpu_temp)
+        if cpu_temp >= MAX_CPU_TEMP:
+            GPIO.output(IMPULS_PIN, True)
+        if cpu_temp < STOP_CPU_TEMP:
+            GPIO.output(IMPULS_PIN, False)
+        time.sleep(SLEEP_TIME)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-	
